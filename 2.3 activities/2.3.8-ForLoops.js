04f1369e5c6f0e07dc85791a0d3579debbb18af8
Tyler Capstick <zzznsewzzz@gmail.com>
2022-02-03 21:10:04 -0600

    section 2.3 activities

---
+++ 2.3 activities/2.3.8-ForLoops.js
@@ -0,0 +1,10 @@
+// ? TO DO: create a for loop that TOTALS the numbers from 1 to 100
+const num = 100;
+let conTot  = 0;
+
+
+
+for (let i = 1; i <= num; i++ ) {
+    conTot += i;
+ } 
+ console.log(conTot);
